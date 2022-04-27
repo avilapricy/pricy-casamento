1b72fae6da2d084d5c2e2601cbcd439ebbf7e1c2
Pricy <pricy.avila@outlook.com>
2022-04-27 17:22:45 -0300

    .

--- src/common/components/Logo/style.js
+++ src/common/components/Logo/style.js
@@ -1,11 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    trasition: transform .2s;
-    &:hover {
-        transform: scale(1.3);
-        trasition: transform .6s;
-    }
     @media (max-width: 480px) {
         font-size: 47px;
         text-align: center;
