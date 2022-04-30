5ccf39428027505460eb334d0e02124fe37f961f
Pricy <pricy.avila@outlook.com>
2022-04-30 12:41:52 -0300

    .

--- src/common/components/AddItemList/index.js
+++ src/common/components/AddItemList/index.js
@@ -1,28 +1,48 @@
 import { Form, Input, InputNumber, Button } from 'antd';
 
-const layout = {
-    labelCol: {
-      span: 8,
-    },
-    wrapperCol: {
-      span: 16,
-    },
-  };
-  const validateMessages = {
-    required: '${label} is required!',
-    types: {
-      Tituo: '${label} is not a valid email!',
-      Info: '${label} is not a valid number!',
-    },
-    Link: {
-      range: '${label} must be between ${min} and ${max}',
-    },
-  };
+const { isOpen, onOpen, onClose } = useDisclosure()
+
+const OverlayOne = () => (
+  <ModalOverlay
+      bg='blackAlpha.300'
+      backdropFilter='blur(50px) hue-rotate(90deg)'
+  />
+)
+const [overlay, setOverlay] = React.useState(<OverlayOne />)
+
+useEffect(() => {
+  function close(e) {
+      if (!e.path.find(target => {
+          if(target.id == "modal-AddItemList") {
+              return true;
+          }
+          return false
+      }))
+          onClose()
+  }
+  if (isOpen) {
+      setTimeout(() => {
+          document.body.addEventListener("click", close)
+      }, 500);
+  }
+  return () => {
+      document.body.removeEventListener("click", close)
+  }
+}, [isOpen])
 
 function AddItemList() {
     return(
-        <Button type="primary"> + </Button>
+      
+    <form action="/send-data-here" method="post">
+      <label for="Titulo">Titulo:</label>
+      <input type="text" id="titulo" name="titulo" />
+      <label for="Info">Info:</label>
+      <input type="info" id="info" name="info" />
+      <label for="Link">Link Imagem:</label>
+      <input type="link" id="link" name="link"></input>
+      <button type="submit">Submit</button>
+  </form>
     )
-}
+      
 
 export default AddItemList;
